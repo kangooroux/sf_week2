960cd609775de83fa83d2223b835ddff0309d131
kangooroux <bousquetalexandrepro@gmail.com>
2020-08-24 12:27:43 +0200

    Ajout de l'entité User et de sa fixture

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends BaseFixture
+{
+    private $encoder;
+
+    /**
+     * UserFixtures constructor.
+     * Dans une classe (autre qu'un controlleur), on peut récupérer des services par autowiring uniquement dans le constructeur
+     */
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function loadData()
+    {
+        // adiministrateur
+        $this->createMany(5, 'user_admin', function (int $i) {
+            $admin = new User();
+            $password = $this->encoder->encodePassword($admin, 'admin' . $i);
+
+            return $admin
+                ->setEmail('admin.' . $i . '@ktest.com')
+                ->setRoles(['ROLE_ADMIN'])
+                ->setPassword($password)
+            ;
+        } );
+
+        // utilisateurs
+        $this->createMany(20, 'user_user', function (int $i) {
+            $user = new User();
+            $password = $this->encoder->encodePassword($user, 'user' . $i);
+
+            return $user
+                ->setEmail('user.' . $i . '@ktest.com')
+                ->setPassword($password)
+                ;
+        } );
+
+    }
+}
